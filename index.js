8a9a028c3490476498990f7ba84a6e6271f1a0d3
codewitharyan-35 <aryan35chauhan@gmail.com>
2025-09-03 17:50:07 +0530

    enabled cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const quizzes = require('./db/quizzes');
 
-const app = express();
+const app = express(); //Creating a Server!!
+app.use(cors());
+
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -9,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/quiz', (req, res) => {
-    res.send(quizzes);
+    res.send(quizzes.data);
 })
 
 app.listen(process.env.PORT || PORT, () => {
