49426219533ce7c90d2dc33713466e8dea618b89
codewitharyan-35 <aryan35chauhan@gmail.com>
2025-09-03 19:13:44 +0530

    auth/login router updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const quizRouter = require('./router/quiz.router');
+const userdata = require('./db/users');
 
 const app = express(); //Creating a Server!!
 app.use(cors());
@@ -16,8 +17,13 @@ app.use('/quiz', quizRouter);
 
 app.post('/auth/login', (req, res) => {
     const { username, password } = req.body;
-    console.log({ username, password });
-    res.json({username, password, message: "got the values!!"})
+    // res.json({username, password, message: "got the values!!"})
+    const isUserVerified = userdata.users.some(user => user.username === username && user.password === password);
+    if(isUserVerified){
+        res.status(200).json({ message: "User Verified!!" });
+    }else{
+        res.status(401).json({ message: "Invalid Credentials!! "});
+    }
 })
 
 app.listen(process.env.PORT || PORT, () => {
